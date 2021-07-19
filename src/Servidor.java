be627fc85df7bf123a9914a783710d6729eaf1b7
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2021-07-19 17:28:33 -0400

    Borradas algunas importaciones inútiles

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,12 +1,6 @@
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Servidor {
